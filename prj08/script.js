47ccac0bae49715a3386fb5bf11b1242411bc614
Muhammad Waqar Khan <63258252+waqarkhan09099@users.noreply.github.com>
2021-07-02 05:36:33 -0700

    project 8 complete
    this project have animation or fetch data.

--- prj08/script.js
+++ prj08/script.js
@@ -1,39 +1,111 @@
 const categorytitle = document.getElementById('foodcategorytitle');
 const categorygallery = document.getElementById('foodgallery');
 const categorydiscription = document.getElementById('fooddiscription');
-const searchbutton=document.getElementById('searchButton');
+const searchbutton = document.getElementById('searchButton');
 const searchbar = document.getElementById('searhMealBar');
 const shufflebutton = document.getElementById('shuffle');
 const submit = document.getElementById('submit');
 
-function mealnamefetch(e){
+function mealnamefetch(e) {
     e.preventDefault();
     const foodSelection = searchbar.value;
-    if(foodSelection){
-        fetch(`https:\\www.themealdb.com/api/json/v1/1/search.php?s=${foodSelection}`)
-        .then(res=>res.json())
-        .then(data=>{
-            if(data.meals===null){
-                categorytitle.innerHTML=`<p>your ${foodSelection} category we can't recognize </P>`        
-            }
-            else{
-                categorygallery.innerHTML=data.meals.map(meals=>`
-                <div><img src=""/></div>
+    categorygallery.innerHTML="";
+    categorydiscription.innerHTML='';
+    if (foodSelection) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodSelection}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                // console.log(data.meals.map(meal=>console.log(meal.strMeal)));
+
+
+                if (data.meals === null) {
+                    categorytitle.innerHTML = `<p>your ${foodSelection} category we can't recognize </P>`
+
+                }
+                else {
+                    categorygallery.innerHTML = data.meals.map(meal => `
+                    <div class="meals">
+                        <img src="${meal.strMealThumb}" alt="${meal.strCategory}"/>
+                        <div class="meal-info" data-mealID="${meal.idMeal}">
+                            <h3>${meal.strMeal}</h3>   
+                        </div>
+                    </div>
                 `)
-            }
-        })
-        categorytitle.innerHTML=`<h2>you are selected this ${foodSelection} Category</h2>`;
+                .join('');
+                }
+
+            })
+
+        categorytitle.innerHTML = `<h2>you are selected this ${foodSelection} Category</h2>`;
     }
-    else{
+    else {
         alert(`Please enter food categories`);
     }
-    console.log(data);
+    // search clear
+    searchbar.value="";
 }
 
-// searchbutton.addEventListener('click',(e)=>{
-//     e.preventDefault();
-//     const foodSelection=searchbar.value;
-//     console.log(foodSelection);
-// })
+fetchdiscription=(mealid)=>{
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid}`)
+    .then(res=>res.json())
+    .then(data=>{
+        const meal=data.meals[0];
+        console.log(meal);
+        ingredient(meal);
+    })
+}
 
-submit.addEventListener('submit',mealnamefetch);
+ingredient=(meal)=>{
+    const foodingredient=[];
+    for(let i=1; i<=20; i++){
+        if(meal[`strIngredient${i}`]){
+            foodingredient.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+            
+        }
+        else{
+            break;
+        }
+    };
+    categorygallery.innerHTML='';
+    categorydiscription.innerHTML=`
+    <div class="selected-meal">
+        <div class="selected-food-img">
+            <h2>${meal.strMeal}</h2>
+            <img src="${meal.strMealThumb}"/>
+        </div>
+        <div class="food-ingredient-conatiner">
+            <h2>Ingredients</h2>
+            <ol class="ingredient-list">
+                ${foodingredient.map(meals=>`<li>${meals}</li>`).join('')}
+            </ol>
+        </div>
+    </div>
+    <div class="food-information">
+        ${meal.strCategory ? `<p class="info">Its made by : ${meal.strCategory}</p>` : ''};
+        ${meal.strArea ? `<p class="info">First Introduce by : ${meal.strArea}</p>` : ''};
+        ${meal.strInstructions ? `<p class="info"><strong>Indtruction :</strong> ${meal.strInstructions}</p>` : ''};
+        
+    </div>
+    `;
+
+}
+
+submit.addEventListener('submit', mealnamefetch);
+categorygallery.addEventListener('click',e=>{
+    const containerpath=e.path.find(item=>{
+        if(item.classList){
+            return item.classList.contains('meal-info');
+        }
+        else{
+            return false
+        }
+    });
+    // console.log(containerpath);
+    if(containerpath){
+        const mealId=containerpath.getAttribute('data-mealID');
+        fetchdiscription(mealId);
+    }
+});
+
+
